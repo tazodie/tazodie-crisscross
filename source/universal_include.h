cc0ede30d71c228dfacf607fdd7b92b28ae9aa07
steven.noonan <steven.noonan@localhost>
2006-09-28 21:57:33 +0000

    Copyright fixed.

--- source/universal_include.h
+++ source/universal_include.h
@@ -39,12 +39,12 @@
 #    define APP_CODENAME    "Technetium"
 #    define APP_VERSION     "0.4.0"
 #    define APP_URL         "http://www.uplinklabs.net/crisscross/"
-#    define APP_COPYRIGHT   "(c) 2006 by Steven Noonan <steven@uplinklabs.net> and Rudolf Olah <omouse@gmail.com>"
+#    define APP_COPYRIGHT   "(c) 2006 by Steven Noonan <steven@uplinklabs.net>, Rudolf Olah <omouse@gmail.com>, Miah Clayton <miah@io-in.com"
 #    define APP_COPYRIGHT_CONSOLE   "(c) 2006 by Steven Noonan <steven@uplinklabs.net>,\n" \
                                     "    Rudolf Olah <omouse@gmail.com>, and Miah Clayton <miah@io-in.com>"
     
 #    define ENABLE_CPUID
-#    define ENABLE_DEBUGLOG
+//#    define ENABLE_DEBUGLOG
 
 //NOTE: By disabling this line, you will not be in compliance with article 2
 //      of the New BSD License. If you disable this line, you must display the
