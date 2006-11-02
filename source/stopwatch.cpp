887d849399bdc9fcb52feb7e39f8f9115d471e19
steven.noonan <steven.noonan@localhost>
2006-11-02 06:35:39 +0000

    Copyright update.

--- source/stopwatch.cpp
+++ source/stopwatch.cpp
@@ -5,8 +5,7 @@
  *                              formerly Codename "Technetium"
  *                             project started August 14, 2006
  *
- * Copyright (c) 2006, Steven Noonan <steven@uplinklabs.net>, Rudolf Olah <omouse@gmail.com>,
- * and Miah Clayton <miah@io-in.com>. All rights reserved.
+ * Copyright (c) 2006 IO.IN Research
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -16,9 +15,9 @@
  *     * Redistributions in binary form must reproduce the above copyright notice, this
  *       list of conditions and the following disclaimer in the documentation and/or other
  *       materials provided with the distribution.
- *     * Neither the name of Uplink Laboratories nor the names of its contributors may be
- *       used to endorse or promote products derived from this software without specific
- *       prior written permission.
+ *     * Neither the name of the I.O. Independent Network nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software without
+ *       specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
--- source/universal_include.h
+++ source/universal_include.h
@@ -5,8 +5,7 @@
  *                              formerly Codename "Technetium"
  *                             project started August 14, 2006
  *
- * Copyright (c) 2006, Steven Noonan <steven@uplinklabs.net>, Rudolf Olah <omouse@gmail.com>,
- * and Miah Clayton <miah@io-in.com>. All rights reserved.
+ * Copyright (c) 2006 IO.IN Research
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -16,9 +15,9 @@
  *     * Redistributions in binary form must reproduce the above copyright notice, this
  *       list of conditions and the following disclaimer in the documentation and/or other
  *       materials provided with the distribution.
- *     * Neither the name of Uplink Laboratories nor the names of its contributors may be
- *       used to endorse or promote products derived from this software without specific
- *       prior written permission.
+ *     * Neither the name of the I.O. Independent Network nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software without
+ *       specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -51,9 +50,7 @@
 #    define APP_VERSION             "0.5.0-RC1"
 #    define APP_BRANCH_AT_VERSION   "0.5.0"
 #    define APP_URL                 "http://www.uplinklabs.net/crisscross/"
-#    define APP_COPYRIGHT           "(c) 2006 by Steven Noonan <steven@uplinklabs.net>, Rudolf Olah <omouse@gmail.com>, Miah Clayton <miah@io-in.com"
-#    define APP_COPYRIGHT_CONSOLE   "(c) 2006 by Steven Noonan <steven@uplinklabs.net>,\n" \
-                                    "    Rudolf Olah <omouse@gmail.com>, and Miah Clayton <miah@io-in.com>"
+#    define APP_COPYRIGHT_CONSOLE   "(c) 2006 by IO.IN Research"
 
 // Disabling these two will save space.
 #    define ENABLE_CPUID
