e7ab5aab827b1c675bca784c845ba05fda0ff808
steven.noonan <steven.noonan@localhost>
2006-11-03 03:03:01 +0000

    Adding a ./configure replacement :)

---
+++ common.mk
@@ -0,0 +1,30 @@
+GCC_MINGW    := $(shell $(CXX) -v 2>&1 | \
+                    grep "mingw" )
+GCC_CYGMING  := $(shell $(CXX) -v 2>&1 | \
+                    grep "cygming" )
+GCC_MAJOR    := $(shell $(CXX) -v 2>&1 | \
+                        grep " version " | cut -d' ' -f3  | cut -d'.' -f1)
+GCC_MINOR    := $(shell $(CXX) -v 2>&1 | \
+                        grep " version " | cut -d' ' -f3  | cut -d'.' -f2)
+
+GCC_ISEMULATED = no
+GCC_ISCYGMING = no
+GCC_ISMINGW = no
+CC_BUILDSTATIC = no
+
+ifneq ($(GCC_MINGW),)
+GCC_ISMINGW = yes
+GCC_ISEMULATED = yes
+endif
+
+ifneq ($(GCC_CYGMING),)
+GCC_ISCYGMING = yes
+GCC_ISEMULATED = yes
+CC_BUILDSTATIC = yes
+endif
+
+ifeq ($(GCC_ISCYGMING),yes)
+FPIC =
+else
+FPIC = -fPIC
+endif
