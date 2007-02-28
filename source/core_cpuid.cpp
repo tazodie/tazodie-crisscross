735e298abedf4e6b41b867d61bc2f1cd9797dd6e
steven.noonan <steven.noonan@localhost>
2007-02-28 00:53:34 +0000

    Updating the copyright date.

--- source/core_cpuid.cpp
+++ source/core_cpuid.cpp
@@ -5,7 +5,7 @@
  *                              formerly Codename "Technetium"
  *                             project started August 14, 2006
  *
- * Copyright (c) 2006 IO.IN Research
+ * Copyright (c) 2006-2007 IO.IN Research
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
