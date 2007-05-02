f58199b56db100c07338a16f82e44159dde11c48
steven.noonan <steven.noonan@localhost>
2007-05-02 07:15:01 +0000

    Readding Wno-long-long because pedantic/ansi are misbehaving in GCC 4.1.2.

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@ CC = gcc
 LINK = $(CXX)
 
 ifeq ($(CFLAGS),)
-CFLAGS = -O3 -march=pentium2 -mtune=pentium2 -Wall -ansi -pedantic -pipe -g
+CFLAGS = -O3 -march=pentium2 -mtune=pentium2 -Wall -ansi -pedantic -Wno-long-long -pipe -g
 endif
 
 ifeq ($(CXXFLAGS),)
