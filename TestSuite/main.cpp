e48530a3fc8ed1fa6a54d1623247ac34c93f934f
steven.noonan <steven.noonan@localhost>
2007-05-14 10:00:09 +0000

    Logic errors corrected, and memory leak corrected (would leak memory if it exited early).

--- TestSuite/main.cpp
+++ TestSuite/main.cpp
@@ -33,37 +33,39 @@ int RunApplication (int argc, char **argv) {
     
 	int retval = 0;
 
-	WritePrefix ( "Size of Uint8" ); retval &= WriteResult ( TestDatatypes_Uint8() );
-	WritePrefix ( "Size of Uint16" ); retval &= WriteResult ( TestDatatypes_Uint16() );
-	WritePrefix ( "Size of Uint32" ); retval &= WriteResult ( TestDatatypes_Uint32() );
-	WritePrefix ( "Size of Uint64" ); retval &= WriteResult ( TestDatatypes_Uint64() );
-	WritePrefix ( "Size of Bool" ); retval &= WriteResult ( TestDatatypes_Bool() );
-	WritePrefix ( "Size of Float" ); retval &= WriteResult ( TestDatatypes_Float() );
-	WritePrefix ( "Size of Double" ); retval &= WriteResult ( TestDatatypes_Double() );
-	WritePrefix ( "Size of void *" ); retval &= WriteResult ( TestDatatypes_Pointer() );
+	WritePrefix ( "Size of Uint8" ); retval |= WriteResult ( TestDatatypes_Uint8() );
+	WritePrefix ( "Size of Uint16" ); retval |= WriteResult ( TestDatatypes_Uint16() );
+	WritePrefix ( "Size of Uint32" ); retval |= WriteResult ( TestDatatypes_Uint32() );
+	WritePrefix ( "Size of Uint64" ); retval |= WriteResult ( TestDatatypes_Uint64() );
+	WritePrefix ( "Size of Bool" ); retval |= WriteResult ( TestDatatypes_Bool() );
+	WritePrefix ( "Size of Float" ); retval |= WriteResult ( TestDatatypes_Float() );
+	WritePrefix ( "Size of Double" ); retval |= WriteResult ( TestDatatypes_Double() );
+	WritePrefix ( "Size of void *" ); retval |= WriteResult ( TestDatatypes_Pointer() );
 
-	WritePrefix ( "DataBuffer Compare" ); retval &= WriteResult ( TestComparison_DataBuffer() );
-	WritePrefix ( "Integer Compare" ); retval &= WriteResult ( TestComparison_Integer() );
-	WritePrefix ( "Unsigned Integer Compare" ); retval &= WriteResult ( TestComparison_UnsignedInteger() );
-	WritePrefix ( "LList" ); retval &= WriteResult ( TestLList() );
-	WritePrefix ( "DArray" ); retval &= WriteResult ( TestDArray() );
-	WritePrefix ( "DStack" ); retval &= WriteResult ( TestDStack() );
+	WritePrefix ( "DataBuffer Compare" ); retval |= WriteResult ( TestComparison_DataBuffer() );
+	WritePrefix ( "Integer Compare" ); retval |= WriteResult ( TestComparison_Integer() );
+	WritePrefix ( "Unsigned Integer Compare" ); retval |= WriteResult ( TestComparison_UnsignedInteger() );
+	WritePrefix ( "LList" ); retval |= WriteResult ( TestLList() );
+	WritePrefix ( "DArray" ); retval |= WriteResult ( TestDArray() );
+	WritePrefix ( "DStack" ); retval |= WriteResult ( TestDStack() );
 
-	WritePrefix ( "RedBlackTree<DataBuffer, DataBuffer *>" ); retval &= WriteResult ( TestRedBlackTree_DataBuffer() );
-	WritePrefix ( "RedBlackTree<std::string, std::string>" ); retval &= WriteResult ( TestRedBlackTree_stdstring() );
-	WritePrefix ( "RedBlackTree<int, int>" ); retval &= WriteResult ( TestRedBlackTree_Int() );
+	WritePrefix ( "RedBlackTree<DataBuffer, DataBuffer *>" ); retval |= WriteResult ( TestRedBlackTree_DataBuffer() );
+	WritePrefix ( "RedBlackTree<std::string, std::string>" ); retval |= WriteResult ( TestRedBlackTree_stdstring() );
+	WritePrefix ( "RedBlackTree<int, int>" ); retval |= WriteResult ( TestRedBlackTree_Int() );
 
-	WritePrefix ( "HeapSort (integer array)" ); retval &= WriteResult ( TestHeapSort_IntArray() );
-	WritePrefix ( "HeapSort (integer DArray)" ); retval &= WriteResult ( TestHeapSort_DArray() );
+	WritePrefix ( "HeapSort (integer array)" ); retval |= WriteResult ( TestHeapSort_IntArray() );
+	WritePrefix ( "HeapSort (integer DArray)" ); retval |= WriteResult ( TestHeapSort_DArray() );
 
-	WritePrefix ( "Stopwatch" ); retval &= WriteResult ( TestStopwatch() );
-	g_console->Flush(); CoreAssert ( retval == 0 );
+	WritePrefix ( "Stopwatch" ); retval |= WriteResult ( TestStopwatch() );
+	g_console->Flush();
 
 #ifdef TARGET_OS_WINDOWS
 	system ( "pause" );
 #endif
 
 	delete g_console;
+
+    CoreAssert ( retval == 0 );
 
 	return 0;
 }
