e79e4d225c744dce5ffb5671a201b4f80dc54456
steven.noonan <steven.noonan@localhost>
2007-05-18 22:16:56 +0000

    char* comparison template specialisation added.

--- source/crisscross/compare.h
+++ source/crisscross/compare.h
@@ -18,11 +18,21 @@ namespace CrissCross
     {
         //! Function for generic data comparison.
         /*!
-            \warning This doesn't work for pointers. Instead of character arrays, use std::string.
             \param _first The item to compare to _second. Must have comparison operators > and < implemented.
             \param _second The item to compare to _first. Must have comparison operators > and < implemented.
             \return 0 if the items are equal, -1 if _first is less than _second, and 1 if _first is greater than _second.
          */
+        template < class T >
+        __inline int Compare ( T &_first, T &_second )
+        {
+            if ( _first < _second )
+                return -1;
+            else if ( _first > _second )
+                return 1;
+            else
+                return 0;
+        }
+		
         template < class T >
         __inline int Compare ( const T &_first, const T &_second )
         {
@@ -32,6 +42,18 @@ namespace CrissCross
                 return 1;
             else
                 return 0;
+        }
+		
+		template <>
+        __inline int Compare<const char *> ( const char * &_first, const char *&_second )
+        {
+			return strcmp ( _first, _second );
+        }
+		
+		template <>
+        __inline int Compare<char *> ( char * &_first, char *&_second )
+        {
+			return strcmp ( _first, _second );
         }
     }
 }
