8e31996fc41e1d15200882d9a234ebdf573208ec
steven.noonan <steven.noonan@localhost>
2007-06-09 05:29:07 +0000

    Correcting builds with 'make' on Intel Macs.

--- common.mk
+++ common.mk
@@ -3,6 +3,8 @@
 VERSION = 0.6.1
 VERSION_MAJOR = 0
 
+OPTLEVEL = 3
+
 ifeq ($(CXX),)
 CXX = g++
 endif
@@ -12,18 +14,6 @@ CC = gcc
 endif
 
 LINK = $(CXX)
-
-ifeq ($(CFLAGS),)
-CFLAGS = -O3 -march=pentium2 -mtune=pentium2 -Wall -ansi -pedantic -Wno-long-long -pipe -ggdb
-else
-CFLAGS := -Wall -ansi -pedantic -Wno-long-long
-endif
-
-ifeq ($(CXXFLAGS),)
-CXXFLAGS = $(CFLAGS)
-else
-CXXFLAGS := -Wall -ansi -pedantic -Wno-long-long
-endif
 
 INCLUDES = 
 
@@ -37,11 +27,23 @@ GCC_MAJOR    := $(shell $(CXX) -v 2>&1 | \
                         grep " version " | cut -d' ' -f3  | cut -d'.' -f1)
 GCC_MINOR    := $(shell $(CXX) -v 2>&1 | \
                         grep " version " | cut -d' ' -f3  | cut -d'.' -f2)
+GCC_PROC     := $(shell uname -m)
 
+GCC_ISAPPLE = no
 GCC_ISEMULATED = no
 GCC_ISCYGMING = no
 GCC_ISMINGW = no
+GCC_ISX86 = no
+GCC_ISPPC = no
 CC_BUILDSTATIC = yes
+
+ifeq ($(GCC_PROC),i386)
+GCC_ISX86 = yes
+endif
+
+ifeq ($(GCC_PROC),ppc)
+GCC_ISPPC = yes
+endif
 
 ifneq ($(GCC_MINGW),)
 GCC_ISMINGW = yes
@@ -65,10 +67,39 @@ else
 FPIC = -fPIC
 endif
 
-ifneq ($(CC_BUILDSTATIC),yes)
-LIBNAME = libCrissCross.so
+ifeq ($(GCC_ISAPPLE),yes)
+    ifeq ($(GCC_ISX86),yes)
+        # Intel Core Duo or Core 2 Duo (Intel Mac)
+        ARCH = -march=prescott -mtune=prescott
+    else
+    ifeq ($(GCC_ISPPC),yes)
+        ARCH = -mtune=g4
+    endif
+    endif
 else
-LIBNAME = libCrissCross-$(VERSION).a
+    ifeq ($(GCC_ISX86),yes)
+        ARCH = -march=pentium2 -mtune=pentium2
+    else
+        ARCH =
+    endif
+endif
+
+ifeq ($(CFLAGS),)
+CFLAGS = -O$(OPTLEVEL) $(ARCH) -Wall -ansi -pedantic -Wno-long-long -pipe -ggdb
+else
+CFLAGS := -Wall -ansi -pedantic -Wno-long-long
+endif
+
+ifeq ($(CXXFLAGS),)
+CXXFLAGS = $(CFLAGS)
+else
+CXXFLAGS := -Wall -ansi -pedantic -Wno-long-long
+endif
+
+ifneq ($(CC_BUILDSTATIC),yes)
+LIBNAME = libCrissCross-$(VERSION).so
+else
+LIBNAME = libCrissCross.a
 endif
 
 AR = ar rc
