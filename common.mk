cde2a709a5ca3cd57e7ee74014e21fe76659c4f8
steven.noonan <steven.noonan@localhost>
2007-08-30 03:27:57 +0000

    Adding some makefile mumbo jumbo to allow architecture detection and prevent certain compilers from optimizing out the GenPrime and isPrime calls.

--- common.mk
+++ common.mk
@@ -1,13 +1,3 @@
-# Some useful functions for numeric comparison.
-
-gt = $(filter-out $(words $2),$(words $(call max,$1,$2)))
-lt = $(filter-out $(words $1),$(words $(call max,$1,$2)))
-eq = $(filter $(words $1),$(words $2))
-ne = $(filter-out $(words $1),$(words $2))
-gte = $(call gt,$1,$2)$(call eq,$1,$2)
-lte = $(call lt,$1,$2)$(call eq,$1,$2)
-
-
 # CrissCross common makefile options.
 
 VERSION = 0.6.5
@@ -38,17 +28,55 @@ GCC_MAJOR    := $(shell $(CXX) -v 2>&1 | \
 GCC_MINOR    := $(shell $(CXX) -v 2>&1 | \
                         grep " version " | cut -d' ' -f3  | cut -d'.' -f2)
 GCC_PROC     := $(shell uname -m)
+GCC_MMX      := $(shell cat /proc/cpuinfo | grep mmx)
+GCC_SSE      := $(shell cat /proc/cpuinfo | grep sse)
+GCC_SSE2     := $(shell cat /proc/cpuinfo | grep sse2)
 
 GCC_ISAPPLE = no
 GCC_ISEMULATED = no
 GCC_ISCYGMING = no
 GCC_ISMINGW = no
-GCC_ISX86 = no
+GCC_ISINTEL = no
+GCC_IS386 = no
+GCC_IS486 = no
+GCC_IS586 = no
+GCC_IS686 = no
 GCC_ISPPC = no
+GCC_HAS_MMX = no
+GCC_HAS_SSE = no
+GCC_HAS_SSE2 = no
 CC_BUILDSTATIC = yes
 
 ifeq ($(GCC_PROC),i386)
-GCC_ISX86 = yes
+GCC_IS386 = yes
+GCC_ISINTEL = yes
+endif
+
+ifeq ($(GCC_PROC),i486)
+GCC_IS486 = yes
+GCC_ISINTEL = yes
+endif
+
+ifeq ($(GCC_PROC),i586)
+GCC_IS586 = yes
+GCC_ISINTEL = yes
+endif
+
+ifeq ($(GCC_PROC),i686)
+GCC_IS686 = yes
+GCC_ISINTEL = yes
+endif
+
+ifneq ($(GCC_MMX),)
+GCC_HAS_MMX = yes
+endif
+
+ifneq ($(GCC_SSE),)
+GCC_HAS_SSE = yes
+endif
+
+ifneq ($(GCC_SSE2),)
+GCC_HAS_SSE2 = yes
 endif
 
 ifeq ($(GCC_PROC),ppc)
@@ -77,26 +105,45 @@ else
 FPIC = -fPIC
 endif
 
+ARCH =
+
 ifeq ($(GCC_ISAPPLE),yes)
-    ifeq ($(GCC_ISX86),yes)
+    ifeq ($(GCC_ISINTEL),yes)
         # Intel Core Duo or Core 2 Duo (Intel Mac)
-        ARCH = -m32 -march=prescott -mtune=prescott
-    else
+        ARCH = -m32 -march=prescott
+    endif
     ifeq ($(GCC_ISPPC),yes)
         ARCH = -mtune=G4
-    else
-        ARCH =
-    endif
     endif
 else
-    ifeq ($(GCC_ISX86),yes)
-        ARCH = -m32 -march=pentium2 -mtune=pentium2
-    else
+    ifeq ($(GCC_IS386),yes)
+        ARCH = -m32 -march=i386
+    endif
+    ifeq ($(GCC_IS486),yes)
+        ARCH = -m32 -march=i486
+    endif
+    ifeq ($(GCC_IS586),yes)
+        ARCH = -m32 -march=pentium
+    endif
+    ifeq ($(GCC_IS686),yes)
+        ARCH = -m32 -march=pentiumpro
+    endif
+    ifeq ($(GCC_HAS_MMX),yes)
+        ARCH = -m32 -march=pentium2
+    endif
+    ifeq ($(GCC_HAS_MMX),yes)
+        ARCH = -m32 -march=pentium2
+    endif
+    ifeq ($(GCC_HAS_SSE),yes)
+        ARCH = -m32 -march=pentium3
+    endif
+    ifeq ($(GCC_MAJOR),4)
+        ifeq ($(GCC_HAS_SSE2),yes)
+            ARCH = -m32 -march=pentium-m -mtune=pentium-m
+        endif
+    endif
     ifeq ($(GCC_ISPPC),yes)
         ARCH = -mtune=G4
-    else
-        ARCH =
-    endif
     endif
 endif
 
