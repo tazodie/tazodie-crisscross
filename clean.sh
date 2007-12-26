96d827ef358c4de6e85fc05af846c9c6291b09ea
steven.noonan <steven.noonan@localhost>
2007-12-26 18:49:33 +0000

    Adding a cleanup script.

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo
+echo "The following files will be purged:"
+echo
+svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'
+echo
+read -p "Continue (Y/N)?"
+if [ "$REPLY" == "y" ]; then
+	rm -rvf `svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'`
+else
+	echo
+	echo "No files removed."
+	echo
+fi
