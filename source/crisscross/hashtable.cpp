05d3cc9a5d297e72d40d9f7aec7a4a0a3f798f1d
steven.noonan <steven.noonan@localhost>
2008-10-03 04:36:24 +0000

    HashTable is now 3 billion percent more useful.

--- source/crisscross/hashtable.cpp
+++ source/crisscross/hashtable.cpp
@@ -15,9 +15,6 @@
 
 #include <crisscross/stopwatch.h>
 
-#define HASH_TABLE_FILL_THRESHHOLD 75.0
-#define HASH_TABLE_SIZE_MAGNITUDE 2
-
 namespace CrissCross
 {
 	namespace Data
@@ -25,29 +22,26 @@ namespace CrissCross
 		template <class T>
 		HashTable<T>::HashTable(size_t _initialSize)
 		{
-			if (_initialSize < 500) _initialSize = 500;
+			if (_initialSize < 25) _initialSize = 25;
 
 			m_size = _initialSize;
 			m_used = 0;
-			m_array = new T[_initialSize];
-			m_keys = new char *[_initialSize];
-			memset(m_array, 0, sizeof(T) * _initialSize);
-			memset(m_keys, 0, sizeof(char *) * _initialSize);
+			m_array = new tree_t *[_initialSize];
+			memset(m_array, 0, sizeof(tree_t *) * _initialSize);
+
 			m_insertions = m_collisions = m_searches = m_hits = 0;
 		}
 
 		template <class T>
 		HashTable<T>::~HashTable()
 		{
+			for (size_t i = 0; i < m_size; i++) {
+				delete m_array[i];
+				m_array[i] = NULL;
+			}
 			delete [] m_array;
 			m_array = NULL;
-			for (size_t i = 0; i < m_size; i++) {
-				delete [] m_keys[i];
-				m_keys[i] = NULL;
-			}
 
-			delete [] m_keys;
-			m_keys = NULL;
 			m_size = 0;
 		}
 
@@ -72,6 +66,7 @@ namespace CrissCross
 		template <class T>
 		void HashTable<T>::grow()
 		{
+			/*
 			size_t  oldSize = m_size;
 			m_size *= HASH_TABLE_SIZE_MAGNITUDE;
 			m_used = 0;
@@ -93,6 +88,7 @@ namespace CrissCross
 
 			delete [] oldArray;
 			delete [] oldKeys;
+			*/
 		}
 
 		template <class T>
@@ -100,60 +96,32 @@ namespace CrissCross
 		{
 			size_t khash = hash(_key, strlen(_key));
 			size_t pos = khash % m_size;
-
-			m_searches++;
-
-			if (!m_keys[pos] || strcmp(m_keys[pos], _key) != 0) {
-				/* Hmm. The data isn't at the location it should be. */
-				size_t collisionNum = 0, attempts = 0;
-
-				while (!m_keys[pos] || strcmp(m_keys[pos], _key) != 0)
-				{
-					pos += 2 * ++collisionNum - 1;
-					if (pos >= m_size)
-						pos %= m_size;
-
-					attempts++;
-					if (attempts > 128)
-						return 0; /* Kills us if something's seriously wrong */
-
-				}
-
-				/* if ( !m_keys[pos] ) */
-				/*    return 0; */
-
-				m_hits++;
-
-				CoreAssert(pos < m_size);
-				CoreAssert(m_keys[pos] != NULL);
-			} else {
-				m_hits++;
-			}
-
 			return pos;
 		}
 
 		template <class T>
-		T const & HashTable<T>::find(const char * const & _key) const
+		T HashTable<T>::find(const char * const & _key) const
 		{
 			static T null(0);
 			size_t   index = findIndex(_key);
-			if (m_keys[index] && strcmp(m_keys[index], _key) == 0)
-				return m_array[index];
-			else
-				return null;
+			if (!m_array[index]) return null;
+			return m_array[index]->find(_key);
+		}
+
+		template <class T>
+		bool HashTable<T>::exists(const char * const & _key) const
+		{
+			size_t   index = findIndex(_key);
+			if (!m_array[index]) return false;
+			return m_array[index]->exists(_key);
 		}
 
 		template <class T>
 		bool HashTable<T>::find(const char * const &_key, T &_data) const
 		{
 			size_t index = findIndex(_key);
-			if (m_keys[index] && strcmp(m_keys[index], _key) == 0) {
-				_data = m_array[index];
-				return true;
-			}
-
-			return false;
+			if (!m_array[index]) return false;
+			return m_array[index]->find(_key, _data);
 		}
 
 		template <class T>
@@ -169,17 +137,17 @@ namespace CrissCross
 			printf("\n");
 			printf("HASH TABLE STATISTICS:\n\n");
 			printf("SIZE: %lu (%luKB)\n", m_size, ((m_size * sizeof(T)) + (m_size * sizeof(char *))) / 1024);
-			printf("USED: %lu (%0.2lf%%)\n", m_used, usagePercentage);
+			printf("USED: %lu (%0.2f%%)\n", m_used, usagePercentage);
 			printf("SEARCH STATISTICS:\n");
 			printf("\tSEARCHES: %lu\n", m_searches);
-			printf("\tHITS: %lu (%0.2lf%%)\n", m_hits, hitPercentage);
+			printf("\tHITS: %lu (%0.2f%%)\n", m_hits, hitPercentage);
 			printf("INSERTION STATISTICS:\n");
 			printf("\tINSERTIONS: %lu\n", m_insertions);
-			printf("\tCOLLISIONS: %lu (%0.2lf%%)\n", m_collisions, collisionPercentage);
+			printf("\tCOLLISIONS: %lu (%0.2f%%)\n", m_collisions, collisionPercentage);
 			double q1 = hitPercentage,
 			       q2 = (100.0 - collisionPercentage),
 			       q3 = (usagePercentage * (100.0 / HASH_TABLE_FILL_THRESHHOLD));
-			printf("*** OVERALL QUALITY ESTIMATION : %0.2lf%% ( (%0.2lf%% + %0.2lf%% + %0.2lf%%) / 3.0 ) ***\n",
+			printf("*** OVERALL QUALITY ESTIMATION : %0.2f%% ( (%0.2f%% + %0.2f%% + %0.2f%%) / 3.0 ) ***\n",
 			       (q1 + q2 + q3) / 3.0, q1, q2, q3);
 			printf("\n");
 
@@ -190,63 +158,34 @@ namespace CrissCross
 		bool HashTable<T>::erase(const char * const &_key)
 		{
 			size_t index = findIndex(_key);
-			if (m_keys[index] && strcmp(m_keys[index], _key) == 0) {
-				delete [] m_keys[index];
-				m_keys[index] = NULL;
-				m_used--;
-				return true;
-			}
-
-			return false;
+			if (!m_array[index]) return false;
+			return m_array[index]->erase(_key);
 		}
 
 		template <class T>
 		size_t HashTable<T>::mem_usage() const
 		{
+			/*
 			size_t ret = sizeof(*this);
 			ret += m_size * sizeof(T);
 			ret += m_size * sizeof(char *);
 			return ret;
+			*/
+			return 0;
 		}
 
 		template <class T>
 		bool HashTable<T>::insert(const char * const &_key, T const &_data)
 		{
-			if ((((double)m_used / (double)m_size) * 100) > HASH_TABLE_FILL_THRESHHOLD) {
-				/* We need to grow and rebuild the table... We're too full. */
-				grow();
-			}
-
 			size_t khash = hash(_key, strlen(_key));
 			size_t pos = khash % m_size;
 
-			m_insertions++;
+			if (!m_array[pos])
+				m_array[pos] = new tree_t();
 
-			if (m_keys[pos] != NULL) {
-				/* Collision! */
-				size_t collisionNum = 0, attempts = 0;
-				while (m_keys[pos])
-				{
-					pos += 2 * ++collisionNum - 1;
-					if (pos >= m_size)
-						pos %= m_size;
+			CoreAssert(m_array[pos]);
 
-					/* Kills us if we didn't resize properly. */
-					attempts++;
-					CoreAssert(attempts < m_size * 2);
-				}
-				m_collisions += collisionNum / 2;
-				CoreAssert(pos < m_size);
-				CoreAssert(m_keys[pos] == NULL);
-			}
-
-			/* Seems OK. */
-			m_array[pos] = _data;
-			m_keys[pos] = new char [strlen(_key) + 1];
-			strcpy(m_keys[pos], _key);
-			m_used++;
-
-			return true;
+			return m_array[pos]->insert(_key, _data);
 		}
 	}
 }
