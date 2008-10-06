29eee94c1b79f7d4d22df8b542123678c1393bf9
steven.noonan <steven.noonan@localhost>
2008-10-06 02:18:55 +0000

    clean.sh: updated for git 1.6
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ echo "The following files will be purged:"
 if [ -d .svn ]; then
 	svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'
 else
-	git-clean -n -f -x -d
+	git clean -n -f -x -d
 fi
 
 echo
@@ -14,7 +14,7 @@ if [ "$REPLY" == "y" ]; then
 	if [ -d .svn ]; then
 		rm -rvf `svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'`
 	else
-		git-clean -f -x -d
+		git clean -f -x -d
 	fi
 else
 	echo
